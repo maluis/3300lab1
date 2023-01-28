23bfe23a71d1f4a57c86aa86495a32e9c5c16c01
maluis <55112120+maluis@users.noreply.github.com>
2023-01-28 01:18:58 -0500

    Add files via upload

---
+++ Program(3300).cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luis Martinez!");
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
